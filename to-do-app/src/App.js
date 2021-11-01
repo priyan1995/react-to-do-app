f3748f1ed3fc2036dedd7279055b6beb06b19241
priyan <45042030+priyan1995@users.noreply.github.com>
2021-11-01 21:24:15 +0530

    bootstrap added

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { ToDoAdd } from './ToDoAdd';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="react-todo-App">
+
+      <div className="text-center">
+        <h2>To Do List</h2>
+      </div>
+
+      <div className="container ">
+        <ToDoAdd />
+      </div>
+
     </div>
   );
 }
---
+++ to-do-app/src/ToDoAdd.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const ToDoAdd = () => {
+
+    return (
+        <>
+
+
+            <div className="row text-center">
+
+            </div>
+
+
+        </>
+    )
+}
