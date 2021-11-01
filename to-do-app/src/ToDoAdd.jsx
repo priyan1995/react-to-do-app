09841aba979f3720d950db2d10f81b7fb35b9a61
priyan <45042030+priyan1995@users.noreply.github.com>
2021-11-01 21:57:53 +0530

    submittion form added

--- to-do-app/src/ToDoAdd.jsx
+++ to-do-app/src/ToDoAdd.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export const ToDoAdd = () => {
+export const ToDoAdd = (props) => {
+
+    const [ input, setInput ] = useState('');
+
+    const handleChange = e => {
+        setInput(e.target.value)
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        console.log('submitted');
+
+        // props.onSubmit({
+        //     id: Math.floor(Math.random()*10000),
+        //     text: input
+        // });
+    };
+
+
+ 
 
     return (
         <>
-
-
             <div className="row text-center">
 
-            </div>
+                <form className='form-control' onSubmit={handleSubmit}>
 
+                    <input 
+                    type='text'
+                    placeholder='add a to-do'
+                    value={input}
+                    name='todoval'
+                    onChange={handleChange}
+                    />
+
+                    <button type='submit'>Submit</button>
+
+                </form>
+
+
+            </div>
 
         </>
     )
