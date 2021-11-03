3324ab0e0688ceb66db93bca4f3e7e75f1da4617
priyan <45042030+priyan1995@users.noreply.github.com>
2021-11-03 08:29:21 +0530

    form submittion added

--- to-do-app/src/components/AddTodo.jsx
+++ to-do-app/src/components/AddTodo.jsx
@@ -3,40 +3,60 @@ import React, { useState } from 'react'
 
 const AddToDo = () => {
 
-    const [todo, setTodo] = useState('');
+
+    const initialData = ({
+        todoId: '',
+        todoItem: ''
+    })
+
+    const [todo, setTodo] = useState(initialData);
 
     const todoForm = document.getElementById('todoForm');
 
     const handleAddTodo = (e) => {
-        setTodo(e.target.value)
+        setTodo({
+            ...todo,
+            todoId: Math.floor(Math.random() * 1000),
+            [e.target.name]: e.target.value.trim()
+            
+        })
     }
 
     const addToDo = (e) => {
-        e.preventDefault()      
-        console.log(todo);
-        setTodo('');    
-        todoForm.reset(); 
-        
+        e.preventDefault()
+        setTodo('');
+        todoForm.reset();
+        console.log(todo)
+
+
+
+
     }
 
-    // props.onSubmit({
-    //     id: Math.floor(Math.random()*10000),
-    //     text: todo
-    // })
 
-  
+
+
 
     return (
         <>
 
             <form onSubmit={addToDo} id="todoForm">
+
+                {/* <input
+                    name="todoId"
+                    type="text"
+                    onChange={handleAddTodo}
+                    value={}
+                    
+                /> */}
+
                 <input
                     type="text"
                     name="todoItem"
                     onChange={handleAddTodo}
                     required
-
                 />
+
                 <button type="submit">Submit</button>
             </form>
 
