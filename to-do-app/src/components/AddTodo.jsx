32d2ed11c1978c53885b00813cd5f294726d6724
priyan <45042030+priyan1995@users.noreply.github.com>
2021-11-03 10:54:02 +0530

    todo app development

--- to-do-app/src/components/AddTodo.jsx
+++ to-do-app/src/components/AddTodo.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 
-
-const AddToDo = () => {
+const AddToDo = (props) => {
 
 
     const initialData = ({
@@ -11,6 +10,8 @@ const AddToDo = () => {
 
     const [todo, setTodo] = useState(initialData);
 
+    
+
     const todoForm = document.getElementById('todoForm');
 
     const handleAddTodo = (e) => {
@@ -18,7 +19,6 @@ const AddToDo = () => {
             ...todo,
             todoId: Math.floor(Math.random() * 1000),
             [e.target.name]: e.target.value.trim()
-            
         })
     }
 
@@ -29,6 +29,8 @@ const AddToDo = () => {
         console.log(todo)
     }
 
+     props = todo
+
 
 
 
@@ -36,8 +38,8 @@ const AddToDo = () => {
     return (
         <>
 
-<h2>Add new item</h2>
-            <form onSubmit={addToDo} id="todoForm">              
+            <h2>Add new item</h2>
+            <form onSubmit={addToDo} id="todoForm">
 
                 <input
                     type="text"
--- to-do-app/src/components/TodoList.jsx
+++ to-do-app/src/components/TodoList.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-export const ToDoList = () => {
+export const ToDoList = (props) => {
 
 
     return(
