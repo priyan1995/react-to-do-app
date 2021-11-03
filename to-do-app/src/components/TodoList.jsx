ccba8aae176322d6fbea7b17b6e0c1951e887ad8
priyan <45042030+priyan1995@users.noreply.github.com>
2021-11-03 14:47:54 +0530

    todo list looped

--- to-do-app/src/components/TodoList.jsx
+++ to-do-app/src/components/TodoList.jsx
@@ -1,13 +1,56 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import FirebaseDb from '../firebase';
 
 export const ToDoList = (props) => {
 
+    const [todos, setTodos] = useState({})
 
-    return(
+
+    useEffect(() => {
+        FirebaseDb.child('to-do').on('value', spanshot => {
+            if (spanshot.val() != null) {
+                setTodos({
+                    ...spanshot.val()
+                })
+            }
+        })
+    }, [])
+
+    return (
         <>
 
-        <h2>To Do List</h2>
+            <h2>To Do List</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>Tasks</td>
+                        <td>Actions</td>
+                    </tr>
+                </thead>
+
+                <tbody>
+
+                    {
+                        Object.keys(todos).map(id=>{
+                            return(
+                                <>
+                                <tr>
+                                    <td >{todos[id].todoId}</td>
+                                    <td >{todos[id].todoItem}</td>
+                                </tr>
+
+                                </>
+                            )
+                        })
+                    }
+
+                </tbody>
+
+
+
+
+            </table>
 
         </>
     )
